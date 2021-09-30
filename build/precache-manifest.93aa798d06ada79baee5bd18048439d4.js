651558bbc976e440bb212439ee70a2f99f790a77
Gnabriel <ls6l9g7g9ud1@opayq.com>
2021-09-30 16:01:33 +0200

    Added frontend

---
+++ build/precache-manifest.93aa798d06ada79baee5bd18048439d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7edf25f41558760d1d5db58d3946d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60f45eb91a3ce1885605",
+    "url": "/static/js/2.db819d7c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.db819d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43dffbb9d1aa039ebc46",
+    "url": "/static/js/main.f8920bfc.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(express.static("build"));
+
 // app.use(morgan("tiny"));
 
 let persons = [
