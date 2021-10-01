3b4c92e0598410cdc327c59c5576dda3f4566e69
Gnabriel <ls6l9g7g9ud1@opayq.com>
2021-10-01 10:30:46 +0200

    Completed up until 3-c Backend connected to a database

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://admin:${password}@cluster0.dc1vt.mongodb.net/phonebook-app?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  important: Boolean,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length == 3) {
+  Person.find({ important: true }).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  const person = new Person({
+    name: name,
+    number: number,
+    important: true,
+  });
+
+  person.save().then((result) => {
+    console.log(`Added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
